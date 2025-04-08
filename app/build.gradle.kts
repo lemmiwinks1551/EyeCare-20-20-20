c69c9f8b2b739be6c1dc7cbbb74f2497a375f5d8
Kirill <ananevka@mail.ru>
2025-04-08 09:20:27 +0300

    minSdk = 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.eyecare20_20_20"
-        minSdk = 28
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
