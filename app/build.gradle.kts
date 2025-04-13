05c6494adc1e14763f3ca6ff8eec0fe02a879893
Kirill <ananevka@mail.ru>
2025-04-13 11:02:43 +0300

    + Room dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
 
     // Jetpack Compose
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.04.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -79,6 +79,12 @@ dependencies {
     // Отладка
     debugImplementation("androidx.compose.ui:ui-tooling") // Визуальный дебаг
     debugImplementation("androidx.compose.ui:ui-test-manifest") // Манипуляции с UI-тестами
+
+    // Room
+    val roomVersion = "2.6.0"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
 }
 
 kapt {
