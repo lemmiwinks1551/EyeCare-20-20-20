4c8aaff20c4c2899af4d0d38dc9e48082397773c
Kirill <ananevka@mail.ru>
2025-04-14 17:05:07 +0300

    unnecessary SDK check removed

--- app/src/main/java/com/example/eyecare20_20_20/EyeCareApp.kt
+++ app/src/main/java/com/example/eyecare20_20_20/EyeCareApp.kt
@@ -12,14 +12,12 @@ class EyeCareApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                "running_channel",
-                "Running notifications",
-                NotificationManager.IMPORTANCE_HIGH
-            )
-            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
+        val channel = NotificationChannel(
+            "running_channel",
+            "Running notifications",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 }
