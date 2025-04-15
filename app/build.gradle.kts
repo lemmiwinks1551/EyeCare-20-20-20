53f4a6b6d8db7852ae19e13e17fc62a102a98c40
Kirill <ananevka@mail.ru>
2025-04-15 19:29:01 +0300

    leakcanary debug Implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,6 +85,9 @@ dependencies {
     implementation("androidx.room:room-runtime:$roomVersion")
     kapt("androidx.room:room-compiler:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
+
+    // LeakCanary
+    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.14")
 }
 
 kapt {
