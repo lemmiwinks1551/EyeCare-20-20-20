7d4bd4ec11c2922d9681cb4be2d167d87b525112
Kirill <ananevka@mail.ru>
2025-04-18 12:46:58 +0300

    new version 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.lemmiwinks.eyecare20_20_20"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
